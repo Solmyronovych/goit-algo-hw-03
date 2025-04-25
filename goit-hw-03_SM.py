c4d8a2493d12f476383aff031d2aee7c3eb10320
Solmyronovych <solmyronovych@gmail.com>
2025-04-25 15:02:59 +0300

    Add files via upload

--- goit-hw-03_SM.py
+++ goit-hw-03_SM.py
@@ -3,8 +3,12 @@ from datetime import datetime
 date = '2020-10-09'
 
 def get_days_from_today(date):
-    specified_date = datetime.strptime(date, '%Y-%m-%d').date()
-    #print(specified_date)
+    try:
+        specified_date = datetime.strptime(date, '%Y-%m-%d').date()
+        #print(specified_date)
+    except ValueError:
+        print (f"Incorrect date format. It should be 'YYYY-MM-DD'")
+        return None
     current_data = datetime.today().date()
     #print(current_data)
     delta = current_data - specified_date
