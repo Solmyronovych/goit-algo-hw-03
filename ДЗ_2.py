fcc6a42519a234a4cb9e426685ed804555f8a8b5
Solmyronovych <solmyronovych@gmail.com>
2025-04-26 13:24:34 +0300

    Add files via upload

--- ДЗ_2.py
+++ ДЗ_2.py
@@ -1,10 +1,16 @@
 from random import randint
 
 def get_numbers_ticket(min, max, quantity):
+    if max < min:
+        return []
+    if min < 1:
+        return []
+    if quantity < 0 or quantity > (max - min):
+        return []
     result_array = set()
     while len(result_array) != quantity:
         result_array.add(randint(min, max))
     return sorted(result_array)
 
-numbers = get_numbers_ticket(1, 1000, 6)
+numbers = get_numbers_ticket(10, 11, 10)
 print(numbers)
